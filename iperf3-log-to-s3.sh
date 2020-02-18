b66b5e25e9352f85a23d0bb8a0d3ff95d98026eb
cyarros10 <christian.yarros@gmail.com>
2020-02-18 14:08:21 -0500

    simple v max

--- iperf3-log-to-s3.sh
+++ iperf3-log-to-s3.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 current_time=$(date "+%Y_%m_%d_%H_%M_%S")
-/bin/iperf3 -c <insert-private-ip-here> -p 5201 --json &>> /iperf/client.json
+/bin/iperf3 -c <insert-private-ip-here> -p 5201 -f M --json &>> /iperf/client.json
 /bin/aws s3 cp /iperf/client.json s3://<insert-s3-bucket-here>/client_$current_time.json
 rm /iperf/client.json
