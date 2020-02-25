727e6c8d4e031cfcb485bc205620cfc0b5ccf478
Christian Yarros <christian.yarros@gmail.com>
2020-02-25 16:20:33 -0500

    Update iperf3-log-to-s3.sh

--- scripts/iperf3-log-to-s3.sh
+++ scripts/iperf3-log-to-s3.sh
@@ -3,7 +3,7 @@
 current_time=$(date "+%Y_%m_%d_%H_%M_%S")
 
 echo "Log: Performing iPerf3 bandwidth test..."
-/bin/iperf3 -c <insert-private-ip-here> -p 5201 -f M --json &>> /iperf/client.json
+/bin/iperf3 -c <insert-private-ip-here> -p 5201 -t 50 -f M --json &>> /iperf/client.json
 
 echo "Log: Sending iPerf3 logfile to S3..."
 /bin/aws s3 cp /iperf/client.json s3://<insert-s3-bucket-here>/client_$current_time.json
