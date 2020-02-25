4386013a1177c632c6e194ee9d66f46a6da58885
Christian Yarros <christian.yarros@gmail.com>
2020-02-25 17:21:24 -0500

    Update iperf3-log-to-s3.sh

--- scripts/iperf3-log-to-s3.sh
+++ scripts/iperf3-log-to-s3.sh
@@ -6,7 +6,8 @@ echo "Log: Performing iPerf3 bandwidth test..."
 /bin/iperf3 -c <insert-private-ip-here> -p 5201 -t 50 -f M --json &>> /iperf/client.json
 
 echo "Log: Sending iPerf3 logfile to S3..."
-/bin/aws s3 cp /iperf/client.json s3://<insert-s3-bucket-here>/client_$current_time.json
+# /bin/aws s3 cp /iperf/client.json s3://<insert-s3-bucket-here>/client_$current_time.json # Amazon Linux
+/usr/local/bin/aws s3 cp /iperf/client.json s3://<insert-s3-bucket-here>/client_$current_time.json # RHEL
 
 echo "Log: Removing temporary log file to maintain storage capacity..."
 rm /iperf/client.json
